81485f2d03d289ed9c35a27e6ab4b14e24cc3323
ClearMind <46052421+ClearMind1@users.noreply.github.com>
2024-11-15 17:33:37 +0800

    fix

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -17,8 +17,8 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '太想伸手摘取星星的人，',
-  HEO_HERO_TITLE_2: '常常忘记脚下的鲜花。',
+  HEO_HERO_TITLE_1: 'Reality',
+  HEO_HERO_TITLE_2: 'Dream',
   HEO_HERO_TITLE_3: 'lump.cc',
   HEO_HERO_TITLE_4: '人生，一半是现实，一半是梦想。',
   HEO_HERO_TITLE_5: '一念花开，一念花落',
@@ -27,9 +27,9 @@ const CONFIG = {
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  //HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
+  //HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
+  //HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
