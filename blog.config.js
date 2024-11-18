64257f30cd98e811144fe90053e58aa53baba2a8
ClearMind <46052421+ClearMind1@users.noreply.github.com>
2024-11-18 09:25:23 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -173,11 +173,11 @@ const BLOG = {
   // 代码主题 @see https://github.com/PrismJS/prism-themes
   PRISM_THEME_PREFIX_PATH:
     process.env.NEXT_PUBLIC_PRISM_THEME_PREFIX_PATH ||
-    'https://cdn.jsdelivr.net/npm/prism-themes@1.9.0/themes/prism-one-light.min.css', // 代码块默认主题
+    'https://cdn.bootcdn.net/ajax/libs/prism-themes/1.9.0/prism-one-light.css', // 代码块默认主题
   PRISM_THEME_SWITCH: process.env.NEXT_PUBLIC_PRISM_THEME_SWITCH || true, // 是否开启浅色/深色模式代码主题切换； 开启后将显示以下两个主题
   PRISM_THEME_LIGHT_PATH:
     process.env.NEXT_PUBLIC_PRISM_THEME_LIGHT_PATH ||
-    'https://cdn.jsdelivr.net/npm/prism-themes@1.9.0/themes/prism-one-light.min.css', // 浅色模式主题
+    'https://cdn.bootcdn.net/ajax/libs/prism-themes/1.9.0/prism-one-light.css', // 浅色模式主题
   PRISM_THEME_DARK_PATH:
     process.env.NEXT_PUBLIC_PRISM_THEME_DARK_PATH ||
     'https://cdn.jsdelivr.net/npm/prismjs@1.29.0/themes/prism-okaidia.min.css', // 深色模式主题
